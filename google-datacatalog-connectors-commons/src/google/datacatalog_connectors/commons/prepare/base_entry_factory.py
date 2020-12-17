a14e7d4dfdd71166ea40d7188e954c791bb7dac9
Marcelo Costa <mesmacosta@gmail.com>
2020-12-17 09:38:37 -0300

    ADD new method to format entry id with hashing (#39)

--- google-datacatalog-connectors-commons/src/google/datacatalog_connectors/commons/prepare/base_entry_factory.py
+++ google-datacatalog-connectors-commons/src/google/datacatalog_connectors/commons/prepare/base_entry_factory.py
@@ -14,26 +14,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import hashlib
 import re
-import unicodedata
 import six
+import unicodedata
 
 
 class BaseEntryFactory:
     __ASCII_CHARACTER_ENCODING = 'ASCII'
+    __ID_MAX_LENGTH = 64
 
     @classmethod
     def _format_id(cls, source_id):
-        formatted_id = re.sub(r'[^a-zA-Z0-9]+', '_',
-                              cls.__normalize_ascii_chars(source_id.strip()))
-        return formatted_id[:64] if len(formatted_id) > 64 else formatted_id
+        formatted_id = cls.__normalize_string(r'[^a-zA-Z0-9]+', source_id)
+
+        return formatted_id[:cls.__ID_MAX_LENGTH] if \
+            len(formatted_id) > cls.__ID_MAX_LENGTH else formatted_id
+
+    @classmethod
+    def _format_id_with_hashing(cls, source_id, hash_length):
+        formatted_id = cls.__normalize_string(r'[^a-zA-Z0-9]+', source_id)
+
+        if len(formatted_id) <= cls.__ID_MAX_LENGTH:
+            return formatted_id
+
+        hash = hashlib.sha1()
+        hash.update(formatted_id.encode(cls.__ASCII_CHARACTER_ENCODING))
+
+        return formatted_id[:cls.__ID_MAX_LENGTH - hash_length] + \
+            hash.hexdigest()[:hash_length]
 
     @classmethod
     def _format_display_name(cls, source_name):
-        formatted_name = re.sub(
-            r'[^a-zA-Z0-9_\- ]+', '_',
-            cls.__normalize_ascii_chars(source_name).strip())
-        return formatted_name
+        return cls.__normalize_string(r'[^\w\- ]+', source_name)
+
+    @classmethod
+    def __normalize_string(cls, regex_pattern, source_string):
+        formatted_str = re.sub(
+            regex_pattern, '_',
+            cls.__normalize_ascii_chars(source_string.strip()))
+        return formatted_str
 
     @classmethod
     def __normalize_ascii_chars(cls, source_string):
