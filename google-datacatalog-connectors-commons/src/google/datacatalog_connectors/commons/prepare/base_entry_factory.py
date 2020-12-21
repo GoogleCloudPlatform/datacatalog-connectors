caec0f56cefa409477f9d35d2dfe881467dce9ae
Marcelo Costa <mesmacosta@gmail.com>
2020-12-21 12:12:49 -0300

    ADD method to truncate and normalize linked resources (#42)

--- google-datacatalog-connectors-commons/src/google/datacatalog_connectors/commons/prepare/base_entry_factory.py
+++ google-datacatalog-connectors-commons/src/google/datacatalog_connectors/commons/prepare/base_entry_factory.py
@@ -19,12 +19,19 @@ import re
 import six
 import unicodedata
 
+from google.datacatalog_connectors.commons import prepare
+
 
 class BaseEntryFactory:
     __ASCII_CHARACTER_ENCODING = 'ASCII'
     __DEFAULT_ENTRY_ID_HASH_LENGTH = 8
     __DEFAULT_ENTRY_ID_INVALID_CHARS_REGEX_PATTERN = r'[^a-zA-Z0-9]+'
     __ID_MAX_LENGTH = 64
+
+    # Linked_resource must contain only letters, numbers, periods, colons,
+    # slashes, underscores, dashes and hashes.
+    __LINKED_RESOURCE_INVALID_CHARS_REGEX_PATTERN = r'[^\w\.\/\-#:]+'
+    __LINKED_RESOURCE_UTF8_MAX_LENGTH = 200
 
     @classmethod
     def _format_id(cls, source_id):
@@ -62,6 +69,30 @@ class BaseEntryFactory:
 
         return formatted_id[:cls.__ID_MAX_LENGTH - hash_length] + \
             hash.hexdigest()[:hash_length]
+
+    @classmethod
+    def _format_linked_resource(cls, linked_resource, normalize=True):
+        """
+        Formats the linked_resource to fit the string bytes limit enforced by
+        Data Catalog, and optionally normalizes it by applying a regex pattern
+        that replaces unsupported characters with underscore.
+
+        Warning: truncating and normalizing linked resources
+        may lead to invalid resulting URLs.
+
+        :param linked_resource: the value to be formatted.
+        :param normalize: enables the normalize logic.
+
+        :return: The formatted linked resource.
+        """
+        formatted_linked_resource = linked_resource
+        if normalize:
+            formatted_linked_resource = cls.__normalize_string(
+                cls.__LINKED_RESOURCE_INVALID_CHARS_REGEX_PATTERN,
+                linked_resource)
+
+        return prepare.DataCatalogStringsHelper.truncate_string(
+            formatted_linked_resource, cls.__LINKED_RESOURCE_UTF8_MAX_LENGTH)
 
     @classmethod
     def _format_display_name(cls, source_name):
--- google-datacatalog-connectors-commons/tests/google/datacatalog_connectors/commons/prepare/base_entry_factory_test.py
+++ google-datacatalog-connectors-commons/tests/google/datacatalog_connectors/commons/prepare/base_entry_factory_test.py
@@ -17,10 +17,14 @@
 
 import unittest
 
+import mock
+
 from google.datacatalog_connectors.commons import prepare
 
 
 class BaseEntryFactoryTestCase(unittest.TestCase):
+    __COMMONS_PACKAGE = 'google.datacatalog_connectors.commons'
+    __PREPARE_PACKAGE = '{}.prepare'.format(__COMMONS_PACKAGE)
 
     def test_format_id_should_normalize_non_compliant_id(self):
         formatted_id = prepare.BaseEntryFactory._format_id(u'ã123 - b456  ')
@@ -53,3 +57,80 @@ class BaseEntryFactoryTestCase(unittest.TestCase):
         formatted_name = prepare.BaseEntryFactory._format_display_name(
             u'ã123 :?: b456  ')
         self.assertEqual('a123 _ b456', formatted_name)
+
+    @mock.patch(
+        '{}.DataCatalogStringsHelper.truncate_string'.format(__PREPARE_PACKAGE)
+    )
+    def test_format_linked_resource_should_not_normalize_compliant_string(
+            self, mock_truncate_string):
+        # Return same value received.
+        mock_truncate_string.side_effect = (lambda *args: args[0])
+
+        formatted_linked_resource = prepare.BaseEntryFactory.\
+            _format_linked_resource(
+                'hdfs://namenode:8020/user/hive/warehouse/table_company'
+                '_names_from_department_that_keeps_records_with_'
+                'historical_data_from_every_single_member')
+
+        self.assertEqual(
+            'hdfs://namenode:8020/user/hive/warehouse/'
+            'table_company_names_from_department_that_'
+            'keeps_records_with_historical_data_'
+            'from_every_single_member', formatted_linked_resource)
+
+    @mock.patch(
+        '{}.DataCatalogStringsHelper.truncate_string'.format(__PREPARE_PACKAGE)
+    )
+    def test_format_linked_resource_should_normalize_non_compliant_string(
+            self, mock_truncate_string):
+        # Return same value received.
+        mock_truncate_string.side_effect = (lambda *args: args[0])
+
+        formatted_linked_resource = prepare.BaseEntryFactory. \
+            _format_linked_resource(
+                'hdfs://[namenode]:8020/user/{hive}/[warehouse]/table_company'
+                '_names_from_?department?_that_;keeps;_records_with_'
+                'historical_data_from_every_single_member')
+
+        self.assertEqual(
+            'hdfs://_namenode_:8020/user/'
+            '_hive_/_warehouse_/table_company_names_from'
+            '__department__that__keeps__records_with_'
+            'historical_data_from_every_single_member',
+            formatted_linked_resource)
+
+    @mock.patch(
+        '{}.DataCatalogStringsHelper.truncate_string'.format(__PREPARE_PACKAGE)
+    )
+    def test_format_linked_resource_should_not_normalize_non_compliant_string(
+            self, mock_truncate_string):
+        # Return same value received.
+        mock_truncate_string.side_effect = (lambda *args: args[0])
+
+        formatted_linked_resource = prepare.BaseEntryFactory. \
+            _format_linked_resource(
+                'hdfs://[namenode]:8020/user/{hive}/[warehouse]/table_company'
+                '_names_from_?department?_that_;keeps;_records_with_'
+                'historical_data_from_every_single_member', False)
+
+        self.assertEqual(
+            'hdfs://[namenode]:8020/user/{hive}/[warehouse]/table_company'
+            '_names_from_?department?_that_;keeps;_records_with_'
+            'historical_data_from_every_single_member',
+            formatted_linked_resource)
+
+    @mock.patch(
+        '{}.DataCatalogStringsHelper.truncate_string'.format(__PREPARE_PACKAGE)
+    )
+    def test_format_linked_resource_should_truncate_non_compliant_string(
+            self, mock_truncate_string):
+        expected_value = 'truncated_str...'
+        mock_truncate_string.return_value = expected_value
+
+        formatted_linked_resource = prepare.BaseEntryFactory. \
+            _format_linked_resource(
+                'hdfs://[namenode]:8020/user/{hive}/[warehouse]/table_company'
+                '_names_from_?department?_that_;keeps;_records_with_'
+                'historical_data_from_every_single_member')
+
+        self.assertEqual(expected_value, formatted_linked_resource)
