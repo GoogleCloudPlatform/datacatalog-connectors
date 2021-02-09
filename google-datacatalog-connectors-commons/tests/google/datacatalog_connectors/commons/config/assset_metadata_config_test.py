b5fca34bc1b2e097d054e6a8eb4f4d3f4d55638d
Marcelo <mesmacosta@gmail.com>
2021-02-09 17:55:44 -0300

    ADD yaml and asset metadata config classes

---
+++ google-datacatalog-connectors-commons/tests/google/datacatalog_connectors/commons/config/assset_metadata_config_test.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from google.datacatalog_connectors.commons import config
+
+import schema
+
+
+class AssetMetadataConfigTestCase(unittest.TestCase):
+
+    def test_parse_as_dict_valid_schema_should_succeed(self):
+        asset_metadata_config = config.AssetMetadataConfig()
+
+        content = '''
+        metadata_definition:
+          name: 'sp_calculateOrder'
+          purpose: 'This stored procedure will calculate orders.'
+          inputs:
+            - name: 'in1'
+              type: 'string'
+          outputs:
+            - name: 'out1'
+              type: 'int'
+        '''
+
+        config_dict = asset_metadata_config.parse_as_dict(content)
+
+        self.assertIsNotNone(config_dict)
+
+    def test_parse_as_dict_missing_optional_attributes_should_succeed(self):
+        asset_metadata_config = config.AssetMetadataConfig()
+
+        content = '''
+        metadata_definition:
+          name: 'sp_calculateOrder'
+          purpose: 'This stored procedure will calculate orders.'
+        '''
+
+        config_dict = asset_metadata_config.parse_as_dict(content)
+
+        self.assertIsNotNone(config_dict)
+
+    def test_parse_as_dict_invalid_attributes_should_raise(self):
+        asset_metadata_config = config.AssetMetadataConfig()
+
+        content = '''
+        metadata_definition:
+          name: 1231
+          purpose: 1233
+          inputs:
+            - name: 'in1'
+              type: 'string'
+          outputs:
+            - name: 'out1'
+              type: 'int'
+        '''
+
+        self.assertRaises(schema.SchemaError,
+                          asset_metadata_config.parse_as_dict, content)
+
+    def test_parse_as_dict_invalid_nested_attributes_should_raise(self):
+        asset_metadata_config = config.AssetMetadataConfig()
+
+        content = '''
+        metadata_definition:
+          name: 'sp_calculateOrder'
+          purpose: 'This stored procedure will calculate orders.'
+          inputs:
+            - name: 123
+              type: 'string'
+          outputs:
+            - name: 'out1'
+              type: 123
+        '''
+
+        self.assertRaises(schema.SchemaError,
+                          asset_metadata_config.parse_as_dict, content)
+
+    def test_parse_as_dict_missing_attributes_should_raise(self):
+        asset_metadata_config = config.AssetMetadataConfig()
+
+        content = '''
+        metadata_definition:
+          inputs:
+            - name: 'in1'
+              type: 'string'
+          outputs:
+            - name: 'out1'
+              type: 'int'
+        '''
+
+        self.assertRaises(schema.SchemaError,
+                          asset_metadata_config.parse_as_dict, content)
