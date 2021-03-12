9fe3d6e621c9e9e0932cc67ba1ede4332aa3275b
mesmacosta <mesmacosta@gmail.com>
2021-03-12 15:12:23 -0300

    UPDATE bump version 0.6.7 -> 0.6.8

--- google-datacatalog-connectors-commons/setup.py
+++ google-datacatalog-connectors-commons/setup.py
@@ -23,7 +23,7 @@ with open('README.md') as readme_file:
 
 setuptools.setup(
     name='google-datacatalog-connectors-commons',
-    version='0.6.7',
+    version='0.6.8',
     author='Google LLC',
     description='Common resources for Data Catalog connectors',
     packages=setuptools.find_packages(where='./src'),
